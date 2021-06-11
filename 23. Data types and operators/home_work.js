7eb224b72ed5f82df6ba9b3f60c4b89904b4971a
Alex Hrabovyi <alexgraboviypro@gmail.com>
2021-06-11 11:54:22 +0300

    Some files was removed from comments

--- 23. Data types and operators/home_work.js
+++ 23. Data types and operators/home_work.js
@@ -1,166 +1,166 @@
 // 1 Запросить у пользователя его возраст и определить, кем он является: ребенком (0–2), подростком (12–18), взрослым (18_60) или пенсионером (60– ...).
 
-// let age = +prompt("Enter your age");
+let age = +prompt("Enter your age");
 
-// if (0 < age && age <= 12) {
-//     alert("You are child");
-// } else if (12 <= age && age < 18) {
-//     alert("You are teenager");
-// } else if (18 <= age && age < 60) {
-//     alert("You are adult");
-// } else if (age >= 60) {
-//     alert("You are pensioner");
-// } else {
-//     alert("You are not born yet");
-// }
+if (0 < age && age <= 12) {
+    alert("You are child");
+} else if (12 <= age && age < 18) {
+    alert("You are teenager");
+} else if (18 <= age && age < 60) {
+    alert("You are adult");
+} else if (age >= 60) {
+    alert("You are pensioner");
+} else {
+    alert("You are not born yet");
+}
 
 // 2 Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д).
 
-// let num = +prompt("Enter a number from 0 to 9");
+let num = +prompt("Enter a number from 0 to 9");
 
-// switch (num) {
-//     case 0:
-//         alert(")");
-//         break;
+switch (num) {
+    case 0:
+        alert(")");
+        break;
 
-//     case 1:
-//         alert("!");
-//         break;
+    case 1:
+        alert("!");
+        break;
 
-//     case 2:
-//         alert("@");
-//         break;
+    case 2:
+        alert("@");
+        break;
 
-//     case 3:
-//         alert("#");
-//         break;
+    case 3:
+        alert("#");
+        break;
 
-//     case 4:
-//         alert("$");
-//         break;
+    case 4:
+        alert("$");
+        break;
 
-//     case 5:
-//         alert("%");
-//         break;
+    case 5:
+        alert("%");
+        break;
 
-//     case 6:
-//         alert("^");
-//         break;
+    case 6:
+        alert("^");
+        break;
 
-//     case 7:
-//         alert("&");
-//         break;
+    case 7:
+        alert("&");
+        break;
 
-//     case 8:
-//         alert("*");
-//         break;
+    case 8:
+        alert("*");
+        break;
 
-//     case 9:
-//         alert("(");
-//         break;
+    case 9:
+        alert("(");
+        break;
 
-//     default:
-//         alert("Incorrect number");
-// }
+    default:
+        alert("Incorrect number");
+}
 
 // 3 Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры.
 
-// let num = +prompt("Enter your number");
+let num = +prompt("Enter your number");
 
-// let first_num = (num - num % 100) / 100;
-// let second_num = (num % 100 - num % 10) / 10;
-// let third_num = num % 10;
-// if (first_num == second_num || first_num == third_num || second_num == third_num) {
-//     alert("This number contains the same numbers");
-// } else {
-//     alert("This number does not have the same numbers");
-// }
+let first_num = (num - num % 100) / 100;
+let second_num = (num % 100 - num % 10) / 10;
+let third_num = num % 10;
+if (first_num == second_num || first_num == third_num || second_num == third_num) {
+    alert("This number contains the same numbers");
+} else {
+    alert("This number does not have the same numbers");
+}
 
 // 4 Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.
 
-// let year = +prompt("Enter a year");
+let year = +prompt("Enter a year");
 
-// if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
-//     alert("This year is a leap year");
-// } else {
-//     alert("This year isn`t a leap year");
-// }
+if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+    alert("This year is a leap year");
+} else {
+    alert("This year isn`t a leap year");
+}
 
 // 5 Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
 
-// let num = +prompt("Enter your number");
+let num = +prompt("Enter your number");
 
-// first_num = (num - num % 10000) / 10000;
-// second_num = (num % 10000 - num % 1000) / 1000;
-// fourth_num = (num % 100 - num % 10) / 10;
-// fifth_num = num % 10;
-// first_num == fifth_num && second_num == fourth_num ? alert("This number is palindrome") : alert("This number isn`t palindrome");
+first_num = (num - num % 10000) / 10000;
+second_num = (num % 10000 - num % 1000) / 1000;
+fourth_num = (num % 100 - num % 10) / 10;
+fifth_num = num % 10;
+first_num == fifth_num && second_num == fourth_num ? alert("This number is palindrome") : alert("This number isn`t palindrome");
 
 // 6 Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести EUR, UAN или AZN, и получает в ответ соответствующую сумму.
 
-// let USD = +prompt("Enter the amount of your USD");
-// let currency = prompt("Enter currency");
+let USD = +prompt("Enter the amount of your USD");
+let currency = prompt("Enter currency");
 
-// switch (currency) {
-//     case "EUR":
-//         alert(USD * 0.82);
-//         break;
+switch (currency) {
+    case "EUR":
+        alert(USD * 0.82);
+        break;
 
-//     case "UAH":
-//         alert(USD * 27.48);
-//         break;
+    case "UAH":
+        alert(USD * 27.48);
+        break;
 
-//     case "AZN":
-//         alert(USD * 1.70);
-//         break;
+    case "AZN":
+        alert(USD * 1.70);
+        break;
 
-//     default:
-//         alert("Incorrect currency");
-// }
+    default:
+        alert("Incorrect currency");
+}
 
 // 7 Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой: от 200 до 300 – скидка будет 3%, от 300 до 500 – 5%, от 500 и выше – 7%.
 
-// let sum = +prompt("Enter purchase amount");
+let sum = +prompt("Enter purchase amount");
 
-// if (sum >= 200 && sum < 300) {
-//     alert(`To pay: ${sum * 0.97}`);
-// } else if (sum >= 300 && sum < 500) {
-//     alert(`To pay: ${sum * 0.95}`);
-// } else if (sum >= 500) {
-//     alert(`To pay: ${sum * 0.93}`)
-// } else {
-//     alert(`To pay: ${sum}`)
-// }
+if (sum >= 200 && sum < 300) {
+    alert(`To pay: ${sum * 0.97}`);
+} else if (sum >= 300 && sum < 500) {
+    alert(`To pay: ${sum * 0.95}`);
+} else if (sum >= 500) {
+    alert(`To pay: ${sum * 0.93}`)
+} else {
+    alert(`To pay: ${sum}`)
+}
 
 // 8 Запросить у пользователя длину окружности и периметр квадрата. Определить, может ли такая окружность поместиться в указанный квадрат.
 
-// let d = +prompt("Enter circumference");
-// let p = +prompt("Enter perimeter of a square");
-// const PI = 3.14;
+let d = +prompt("Enter circumference");
+let p = +prompt("Enter perimeter of a square");
+const PI = 3.14;
 
-// if (d / PI == p / 4) {
-//     alert("A circle can be inscribed in a square");
-// } else {
-//     alert("A circle can`t be inscribed in a square");
-// }
+if (d / PI == p / 4) {
+    alert("A circle can be inscribed in a square");
+} else {
+    alert("A circle can`t be inscribed in a square");
+}
 
 // 9 Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. После вопросов выведите пользователю количество набранных баллов.
 
-// let q_1 = +prompt("2 + 2 = ?");
-// let q_2 = +prompt("x + 2 = 0, x = ?");
-// let q_3 = +prompt("9 / 3 = ?");
+let q_1 = +prompt("2 + 2 = ?");
+let q_2 = +prompt("x + 2 = 0, x = ?");
+let q_3 = +prompt("9 / 3 = ?");
 
-// if (q_1 == 4 && q_2 == -2 && q_3 == 3) {
-//     points = 6;
-// } else if (q_1 == 4 && q_2 == -2 || q_1 == 4 && q_3 == 3 || q_2 == -2 && q_3 == 3) {
-//     points = 4;
-// } else if (q_1 == 4 || q_2 == -2 || q_3 == 3) {
-//     points = 2;
-// } else {
-//     points = 0;
-// }
+if (q_1 == 4 && q_2 == -2 && q_3 == 3) {
+    points = 6;
+} else if (q_1 == 4 && q_2 == -2 || q_1 == 4 && q_3 == 3 || q_2 == -2 && q_3 == 3) {
+    points = 4;
+} else if (q_1 == 4 || q_2 == -2 || q_3 == 3) {
+    points = 2;
+} else {
+    points = 0;
+}
 
-// alert(points);
+alert(points);
 
 // 10 Запросить дату (день, месяц, год) и вывести следующую за ней дату. Учтите возможность перехода на следующий месяц, год, а также високосный год.
 
@@ -173,69 +173,69 @@
 // красоты), если такого нет, то просто выводится как есть. 
 
 
-// let d = +prompt("Enter the day");
-// let m = +prompt("Enter the month");
-// let y = +prompt("Enter the year");
+let d = +prompt("Enter the day");
+let m = +prompt("Enter the month");
+let y = +prompt("Enter the year");
 
-// let next_d = d;
-// let next_m = m;
-// let next_y = y;
-// // if year is leap, year_type = 1; if not year_type = 0
+let next_d = d;
+let next_m = m;
+let next_y = y;
+// if year is leap, year_type = 1; if not year_type = 0
 
-// if (y % 400 == 0 || y % 4 == 0 && y % 100 != 0) {
-//     year_type = 1;
-// } else {
-//     year_type = 0
-// }
+if (y % 400 == 0 || y % 4 == 0 && y % 100 != 0) {
+    year_type = 1;
+} else {
+    year_type = 0
+}
 
-// // if in month 30 days, month_type = 0; if 31, month_type = 1
+// if in month 30 days, month_type = 0; if 31, month_type = 1
 
-// if (m % 2 == 0) {
-//     month_type = 0;
-// } else {
-//     month_type = 1;
-// }
+if (m % 2 == 0) {
+    month_type = 0;
+} else {
+    month_type = 1;
+}
 
-// // -----------
+// -----------
 
-// if (d >= 29 && m == 2 && year_type == 0) {
-//     alert("Incorrect date");
-// } else if (d == 31 && month_type == 0) {
-//     alert("Incorrect date");
-// } else if (d <= 31 && d >= 1 && m <= 12 && m >= 1) {
+if (d >= 29 && m == 2 && year_type == 0) {
+    alert("Incorrect date");
+} else if (d == 31 && month_type == 0) {
+    alert("Incorrect date");
+} else if (d <= 31 && d >= 1 && m <= 12 && m >= 1) {
 
-//     if (d == 31) {
-//         next_d = 1;
-//         next_m = m + 1;
-//     } else if (d == 30 && month_type == 0) {
-//         next_d = 1;
-//         next_m = m + 1
-//     } else if (d == 29 && year_type == 1) {
-//         next_d = 1;
-//         next_m = m + 1
-//     } else if (d == 28 && year_type == 0) {
-//         next_d = 1;
-//         next_m = m + 1
-//     } else {
-//         next_d = d + 1;
-//     }
+    if (d == 31) {
+        next_d = 1;
+        next_m = m + 1;
+    } else if (d == 30 && month_type == 0) {
+        next_d = 1;
+        next_m = m + 1
+    } else if (d == 29 && year_type == 1) {
+        next_d = 1;
+        next_m = m + 1
+    } else if (d == 28 && year_type == 0) {
+        next_d = 1;
+        next_m = m + 1
+    } else {
+        next_d = d + 1;
+    }
 
-//     if (next_m == 13) {
-//         next_m = 1;
-//         next_y = y + 1;
-//     }
+    if (next_m == 13) {
+        next_m = 1;
+        next_y = y + 1;
+    }
 
-//     if (next_d < 10 && next_m < 10) {
-//         alert(`Next day date is 0${next_d}.0${next_m}.${next_y}`);
-//     } else if (next_d < 10) {
-//         alert(`Next day date is 0${next_d}.${next_m}.${next_y}`);
-//     } else if (next_m < 10) {
-//         alert(`Next day date is ${next_d}.0${next_m}.${next_y}`);
-//     } else {
-//         alert(`Next day date is ${next_d}.${next_m}.${next_y}`);
-//     }
+    if (next_d < 10 && next_m < 10) {
+        alert(`Next day date is 0${next_d}.0${next_m}.${next_y}`);
+    } else if (next_d < 10) {
+        alert(`Next day date is 0${next_d}.${next_m}.${next_y}`);
+    } else if (next_m < 10) {
+        alert(`Next day date is ${next_d}.0${next_m}.${next_y}`);
+    } else {
+        alert(`Next day date is ${next_d}.${next_m}.${next_y}`);
+    }
 
-// } else {
-//     alert("Incorrect date");
-// }
+} else {
+    alert("Incorrect date");
+}
 
--- 24. Cycles/home_work.js
+++ 24. Cycles/home_work.js
@@ -1,176 +1,176 @@
 // 1 Подсчитать сумму всех чисел в заданном пользователем диапазоне.
 
-// let num_1 = +prompt("Enter the first number");
-// let num_2 = +prompt("Enter the second number");
-// let sum = 0;
+let num_1 = +prompt("Enter the first number");
+let num_2 = +prompt("Enter the second number");
+let sum = 0;
 
-// let num_biggest = num_1 > num_2 ? num_1 : num_2;
-// let num_smallest = num_1 < num_2 ? num_1 : num_2;
+let num_biggest = num_1 > num_2 ? num_1 : num_2;
+let num_smallest = num_1 < num_2 ? num_1 : num_2;
 
-// for (i = num_smallest; i <= num_biggest; i++) {
-//     sum += i;
-// }
+for (i = num_smallest; i <= num_biggest; i++) {
+    sum += i;
+}
 
-// alert(sum);
+alert(sum);
 
 // 2 Запросить 2 числа и найти только наибольший общий делитель.
 
-// let num_1 = +prompt("Enter the first number");
-// let num_2 = +prompt("Enter the second number");
-// let del;
+let num_1 = +prompt("Enter the first number");
+let num_2 = +prompt("Enter the second number");
+let del;
 
-// for (i = 1; i <= num_1 || i <= num_2; i++) {
-//     if (num_1 % i == 0 && num_2 % i == 0) {
-//         del = i;
-//     }
-// }
+for (i = 1; i <= num_1 || i <= num_2; i++) {
+    if (num_1 % i == 0 && num_2 % i == 0) {
+        del = i;
+    }
+}
 
-// alert(del);
+alert(del);
 
 // 3 Запросить у пользователя число и вывести все делители этого числа.
 
-// let num = +prompt("Enter the number");
-// let message = "";
+let num = +prompt("Enter the number");
+let message = "";
 
-// for (i = 1; i <= num; i++) {
-//     if (num % i == 0) {
-//         message += `${String(i)} `;
-//     }
-// }
+for (i = 1; i <= num; i++) {
+    if (num % i == 0) {
+        message += `${String(i)} `;
+    }
+}
 
-// alert(message);
+alert(message);
 
 // 4 Определить количество цифр в введенном числе.
 
-// let num = +prompt("Enter your number");
-// let amount = 1;
+let num = +prompt("Enter your number");
+let amount = 1;
 
-// for (i = 10; i <= num; i *= 10) {
-//     if (num % i != 0) {
-//         amount++;
-//     }
-// }
+for (i = 10; i <= num; i *= 10) {
+    if (num % i != 0) {
+        amount++;
+    }
+}
 
-// alert(`In your number there are ${amount} digit(s)`);
+alert(`In your number there are ${amount} digit(s)`);
 
 // 5 Запросить у пользователя 10 чисел и подсчитать, сколько он ввел положительных, отрицательных и нулей. 
 // При этом также посчитать, сколько четных и нечетных. Вывести статистику на экран. 
 // Учтите, что достаточно одной переменной (не 10) для ввода чисел пользователем.
 
-// let pos = 0;
-// let neg = 0;
-// let zero = 0;
-// let even = 0;
-// let odd = 0;
+let pos = 0;
+let neg = 0;
+let zero = 0;
+let even = 0;
+let odd = 0;
 
 
-// for (i = 0; i < 10; i++) {
-//     num = +prompt("Enter your number");
+for (i = 0; i < 10; i++) {
+    num = +prompt("Enter your number");
 
-//     if (num > 0) {
-//         pos++;
-//     } else if (num == 0) {
-//         zero++;
-//     } else {
-//         neg++;
-//     }
+    if (num > 0) {
+        pos++;
+    } else if (num == 0) {
+        zero++;
+    } else {
+        neg++;
+    }
 
-//     if (num % 2 == 0) {
-//         even++;
-//     } else {
-//         odd++;
-//     }
-// }
+    if (num % 2 == 0) {
+        even++;
+    } else {
+        odd++;
+    }
+}
 
-// alert(`Positive numbers - ${pos}, negative numbers - ${neg}, zeros - ${zero}; even - ${even}, odd - ${odd}.`);
+alert(`Positive numbers - ${pos}, negative numbers - ${neg}, zeros - ${zero}; even - ${even}, odd - ${odd}.`);
 
 // 6 Зациклить калькулятор. 
 // Запросить у пользователя 2 числа и знак, решить пример, вывести результат и спросить, хочет ли он решить еще один пример.
 // И так до тех пор, пока пользователь не откажется.
 
-// for (i = 0; i != 1;) {
-//     num_1 = +prompt("Enter the first number");
-//     num_2 = +prompt("Enter the second number");
-//     sign = prompt("Enter math operation sign");
+for (i = 0; i != 1;) {
+    num_1 = +prompt("Enter the first number");
+    num_2 = +prompt("Enter the second number");
+    sign = prompt("Enter math operation sign");
 
-//     switch (sign) {
-//         case "+":
-//             alert(`${num_1} + ${num_2} = ${num_1 + num_2}`);
-//             break;
+    switch (sign) {
+        case "+":
+            alert(`${num_1} + ${num_2} = ${num_1 + num_2}`);
+            break;
 
-//         case "-":
-//             alert(`${num_1} - ${num_2} = ${num_1 - num_2}`);
-//             break;
+        case "-":
+            alert(`${num_1} - ${num_2} = ${num_1 - num_2}`);
+            break;
 
-//         case "*":
-//             alert(`${num_1} * ${num_2} = ${num_1 * num_2}`);
-//             break;
+        case "*":
+            alert(`${num_1} * ${num_2} = ${num_1 * num_2}`);
+            break;
 
-//         case "/":
-//             alert(`${num_1} / ${num_2} = ${num_1 / num_2}`);
-//             break;
+        case "/":
+            alert(`${num_1} / ${num_2} = ${num_1 / num_2}`);
+            break;
 
-//         default:
-//             alert("Incorrect sign")
-//     }
+        default:
+            alert("Incorrect sign")
+    }
 
-//     question = prompt("One more operation? [YES/NO]")
+    question = prompt("One more operation? [YES/NO]")
 
-//     if (question == "YES") {
-//         i = 0;
-//     } else {
-//         i = 1;
-//     }
-// }
+    if (question == "YES") {
+        i = 0;
+    } else {
+        i = 1;
+    }
+}
 
 // 8 Зациклить вывод дней недели таким образом: «День недели. Хотите увидеть следующий день?» и так до тех пор, пока пользователь нажимает OK.
 
-// for (i = 1; ; i++) {
-//     switch (i) {
-//         case 1:
-//             alert("Monday");
-//             break;
+for (i = 1; ; i++) {
+    switch (i) {
+        case 1:
+            alert("Monday");
+            break;
 
-//         case 2:
-//             alert("Tuesday");
-//             break;
+        case 2:
+            alert("Tuesday");
+            break;
 
-//         case 3:
-//             alert("Wednesday");
-//             break;
+        case 3:
+            alert("Wednesday");
+            break;
 
-//         case 4:
-//             alert("Thursday");
-//             break;
+        case 4:
+            alert("Thursday");
+            break;
 
-//         case 5:
-//             alert("Friday");
-//             break;
+        case 5:
+            alert("Friday");
+            break;
 
-//         case 6:
-//             alert("Saturday");
-//             break;
+        case 6:
+            alert("Saturday");
+            break;
 
-//         case 7:
-//             alert("Sunday");
-//             break;
+        case 7:
+            alert("Sunday");
+            break;
 
-//         default:
-//             i = 0;
-//     }
-// }
+        default:
+            i = 0;
+    }
+}
 
 // 9 Вывести таблицу умножения для всех чисел от 2 до 9. Каждое число необходимо умножить на числа от 1 до 10.
 
-// let result = "";
+let result = "";
 
-// for (i = 2; i <= 9; i++) {
-//     for (k = 1; k <= 10; k++) {
-//         result += `${String(i * k)} `;
-//     }
-// }
+for (i = 2; i <= 9; i++) {
+    for (k = 1; k <= 10; k++) {
+        result += `${String(i * k)} `;
+    }
+}
 
-// alert(result);
+alert(result);
 
 // 10 Игра «Угадай число».Предложить пользователю загадать число от 0 до 100 и отгадать его следующим способом: 
 // каждую итерацию цикла делите диапазон чисел пополам, записываете результат в N и спрашиваете у пользователя 
